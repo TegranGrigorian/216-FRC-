20d0e4bacf8886e19f1b22636afa8497cf739827
TegranG <tegrgrigor@gmail.com>
2024-02-25 13:48:30 -0500

    auto v1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -79,7 +79,7 @@ public final class Constants {
 
         /* Swerve Profiling Values */
         /** Meters per Second */
-        public static final double maxSpeed = 3.5; //TODO: This must be tuned to specific robot
+        public static final double maxSpeed = 5.5; //TODO: This must be tuned to specific robot
         /** Radians per Second */
         public static final double maxAngularVelocity = 5.0; //TODO: This must be tuned to specific robot
 
